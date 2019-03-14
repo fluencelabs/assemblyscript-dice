5bfcb3235262560fbeb56b8fe8a3909878270cf6
DieMyst <dmitry.shakhtarin@fluence.ai>
2019-03-14 18:50:36 +0300

    structs

--- assembly/index.ts
+++ assembly/index.ts
@@ -43,6 +43,51 @@ export function invoke(ptr: i32, size: i32): i32 {
     return addr;
 }
 
+class JoinRequest {}
+class RollRequest {
+    public player_id: u64;
+    public bet_placement: u8;
+    public bet_size: u32;
+    constructor(player_id: u64 ,bet_placement: u8, bet_size: u32) {
+        this.player_id = player_id;
+        this.bet_placement = bet_placement;
+        this.bet_size = bet_size;
+    }
+}
+class GetBalanceRequest {
+    public player_id: u64;
+    constructor(player_id: u64) {
+        this.player_id = player_id;
+    }
+}
+
+class JoinResponse {
+    player_id: u64;
+    constructor(player_id: u64) {
+        this.player_id = player_id;
+    }
+}
+class RollResponse {
+    public outcome: u8;
+    public player_balance: u64;
+    constructor(outcome: u64 ,player_balance: u8, bet_size: u32) {
+        this.outcome = outcome;
+        this.player_balance = player_balance;
+    }
+}
+class GetBalanceResponse {
+    public player_balance: u64;
+    constructor(player_balance: u64) {
+        this.player_balance = player_balance;
+    }
+}
+class ErrorResponse {
+    message: string;
+    constructor(message: string) {
+        this.message = message;
+    }
+}
+
 class ToDictJSONEventsHandler extends JSONHandler {
 
     public strings: Map<string, string> = new Map();
